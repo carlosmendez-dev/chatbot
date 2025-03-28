cae33784e35626b0756832ce9a09f58d5eb33500
carlos mendez <181876093+carlosmendez-dev@users.noreply.github.com>
2025-03-28 10:48:14 -0600

    se creó la migracion, modelo y controlador de Shell, y se agrego el campo a la migracion y modelo de Command

--- app/Models/Command.php
+++ app/Models/Command.php
@@ -2,13 +2,18 @@
 
 namespace App\Models;
 use App\Models\Tag;
+use App\Models\Shell;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Command extends Model
 {
-    protected $with = ["tag"];
-    
+    protected $with = ["tag","shell"];
+
+    public function shell(){
+        return $this->belongsTo(Shell::class);
+    }
+
     public function tag(){
         return $this->belongsTo(Tag::class);
     }
--- database/migrations/2025_03_28_151635_create_shells_table.php
+++ database/migrations/2025_03_28_151635_create_shells_table.php
@@ -11,16 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('commands', function (Blueprint $table) {
+        Schema::create('shells', function (Blueprint $table) {
             $table->id();
             $table->string("name");
-            $table->unsignedBigInteger("shell_id");
-            $table->unsignedBigInteger("tag_id");
-            $table->string("explanation");
+            $table->string("description");
             $table->timestamps();
-
-            $table->foreign("shell_id")->references("id")->on("shells")->onDelete("cascade");
-            $table->foreign("tag_id")->references("id")->on("tags")->onDelete("cascade");
         });
     }
 
@@ -29,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('commands');
+        Schema::dropIfExists('shells');
     }
 };
