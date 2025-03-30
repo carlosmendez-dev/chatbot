ee02f11722b86c0235f8022de5f524fe9d2e0619
carlos mendez <181876093+carlosmendez-dev@users.noreply.github.com>
2025-03-29 23:35:37 -0600

    se creó el controlador de Home y se agregó al archivo web.php

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+// controllers
+use App\Http\Controllers\HomeController;
+
 Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('dashboard',[HomeController::class,"index"])->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
